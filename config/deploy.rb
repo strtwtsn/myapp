ddca7dfebe435b0a0d630a17c4006c71c13e91d7
Stuart Watson <stuart.watson@unep-wcmc.org>
2017-05-02 11:16:41 +0100

    Second deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,61 @@
-# config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.8.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'myapp'
+set :repo_url, 'git@github.com:strtwtsn/myapp.git'
+
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
+
+set :deploy_user, 'wcmc'
+
+
+set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
+
+
+
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+set :scm_username, "unepwcmc-read"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.4'
+
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+#set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+set :linked_files, %w{config/database.yml .env} 
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
+set :passenger_restart_with_touch, false
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,25 @@
+set :stage, :staging
+set :branch, "master"
+
+
+server "178.79.184.157", user: 'wcmc', roles: %w{app web db}
+
+set :application, "myapp"
+set :server_name, "178.79.184.157"
+set :sudo_user, "wcmc"
+
+
+
+
+
+
+
+
+
+
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
