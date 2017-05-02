68fcc65c3be9c95b25749f21a3b6555eae38d6bb
Stuart Watson <stuart.watson@unep-wcmc.org>
2017-05-02 11:24:10 +0100

    Added db sync

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.8.1'
+lock '3.5.0'
 
 set :application, 'myapp'
 set :repo_url, 'git@github.com:strtwtsn/myapp.git'
@@ -46,7 +46,7 @@ set :pty, true
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-set :linked_files, %w{config/database.yml .env} 
+#set :linked_files, %w{config/database.yml .env} 
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
