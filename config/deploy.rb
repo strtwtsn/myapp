dcf48d3ca0447ae45eaf204f961ddd1bf19bebcb
Stuart Watson <stuart.watson@unep-wcmc.org>
2017-05-02 11:31:49 +0100

    Added db server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.5.0'
+lock '3.8.1'
 
 set :application, 'myapp'
 set :repo_url, 'git@github.com:strtwtsn/myapp.git'
@@ -46,7 +46,7 @@ set :pty, true
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-#set :linked_files, %w{config/database.yml .env} 
+set :linked_files, %w{config/database.yml} 
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -59,3 +59,31 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 set :passenger_restart_with_touch, false
+
+
+# if you haven't already specified
+
+
+# if you want to remove the local dump file after loading
+set :db_local_clean, true
+
+# if you want to remove the dump file from the server after downloading
+set :db_remote_clean, true
+
+# if you want to exclude table from dump
+set :db_ignore_tables, []
+
+# if you want to exclude table data (but not table schema) from dump
+set :db_ignore_data_tables, []
+
+# configure location where the dump file should be created
+set :db_dump_dir, "./db"
+
+# If you want to import assets, you can change default asset dir (default = system)
+# This directory must be in your shared directory on the server
+
+# if you are highly paranoid and want to prevent any push operation to the server
+set :disallow_pushing, false
+
+# if you prefer bzip2/unbzip2 instead of gzip
+set :compressor, :bzip2
